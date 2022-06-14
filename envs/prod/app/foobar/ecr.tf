7f4159c63e116e54006ffc69925cfc21ba84eda2
yuto isogai <jandp.0717@gmail.com>
2022-06-14 22:34:04 +0900

    ecr.tf add modules resource

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -28,3 +28,9 @@ resource "aws_ecr_lifecycle_policy" "nginx" {
 
   repository = aws_ecr_repository.nginx.name
 }
+
+module "nginx" {
+  source = "../../../../modules/ecr"
+
+  name = "example-prod-foobar-nginx"
+}
